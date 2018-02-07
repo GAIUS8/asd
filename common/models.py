6ebbfa2fbb087f1ecd74c29c45e9f081933fa795
GAIUS8 <gaius827@gmail.com>
2018-02-07 23:30:44 +0900

    data 저장용 model 생성, API수신후 저장하는 로직 추가

--- common/models.py
+++ common/models.py
@@ -15,3 +15,30 @@ class DataSetIndex(models.Model):
         db_table = 'dataset_index'
         verbose_name = 'Data Index'
         verbose_name_plural = verbose_name
+
+
+class CropEnvironment(models.Model):
+    date_local = models.DateField(blank=True, null=True)
+    area_id = models.CharField(max_length=100, blank=True, null=True)
+    area_name = models.CharField(max_length=100, blank=True, null=True)
+    crop_id = models.CharField(max_length=100, blank=True, null=True)
+    crop_name = models.CharField(max_length=100, blank=True, null=True)
+    crop_specific = models.CharField(max_length=100, blank=True, null=True)
+    daily_temp_max = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True, verbose_name='일 최고기온')
+    daily_temp_avg = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True, verbose_name='일 평균기온')
+    daily_temp_min = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True, verbose_name='일 최저기온')
+    daily_rmh_avg = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True, verbose_name='일 평균 상대습도')
+    daily_rmh_min = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True, verbose_name='일 최저 상대습도')
+    daily_precipitation = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True, verbose_name='일 강수량')
+    daily_wind_speed_avg = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True, verbose_name='일 평균 풍속')
+    daily_accumulated_sunset = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True, verbose_name='일 누적 일조시간')
+
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    
+    class Meta:
+        db_table = 'crop_environment'
+        verbose_name = '작물별 환경'
+        verbose_name_plural = verbose_name
+        
+        index_together = ['date_local', 'area_id', 'crop_id']
--- receiver/management/commands/request_test.py
+++ receiver/management/commands/request_test.py
@@ -1,9 +1,12 @@
 from django.core.management.base import BaseCommand
-
-import requests
-from bs4 import BeautifulSoup
-
 from django.conf import settings
+
+from bs4 import BeautifulSoup
+import requests
+import datetime
+
+from common.models import CropEnvironment
+from decimal import Decimal
 
 
 class Command(BaseCommand):
@@ -13,25 +16,61 @@ class Command(BaseCommand):
                    'ST_YMD={start_date}&' \
                    'ED_YMD={end_date}&' \
                    'AREA_ID={area_id}&' \
-                   'PA_CROP_NAME={crop_name}&' \
                    'PA_CROP_SPE_ID={crop_id}&' \
-                   'pageNo={page_number}&' \
-                   'numOfRows={num_of_row}'
+        
+        last_date = datetime.date(year=2018, month=1, day=31)
+        date = datetime.date(year=2001, month=1, day=1)
+        
+        print(date)
         
         prams = dict(
             service_key=settings.API_SERVICE_KEY,
-            start_date='20180110',
-            end_date='20180111',
-            area_id='4827000001',
-            crop_name='감자',
+            start_date='20010101',
+            end_date='20010110',
+            area_id='4215000002',
             crop_id='PA020101',
-            page_number=1,
-            num_of_row=10
         )
         
         url = base_url.format(**prams)
         
         response = requests.get(url)
+        print(response.text)
         soup = BeautifulSoup(response.text, 'lxml')
+        items = soup.find_all('item')
         
-        print(soup.find('dayavgws'))
+        db_prams = dict()
+        for daily_data in items:
+            print(daily_data)
+            date_local = daily_data.find('ymd').text.split(' ')[0]
+            area_id = daily_data.find('areaid').text
+            area_name = daily_data.find('areaname').text
+            crop_id = daily_data.find('pacropspeid').text
+            crop_name = daily_data.find('pacropname').text
+            crop_specific = daily_data.find('pacropspename').text
+            daily_temp_max = Decimal(daily_data.find('daymaxta').text)
+            daily_temp_avg = Decimal(daily_data.find('dayavgta').text)
+            daily_temp_min = Decimal(daily_data.find('dayminta').text)
+            daily_rmh_avg = Decimal(daily_data.find('dayavgrhm').text)
+            daily_rmh_min = Decimal(daily_data.find('dayminrhm').text)
+            daily_precipitation = Decimal(daily_data.find('daysumrn').text)
+            daily_wind_speed_avg = Decimal(daily_data.find('dayavgws').text)
+            daily_accumulated_sunset = Decimal(daily_data.find('daysumss').text)
+            
+            db_prams.update(
+                date_local=date_local,
+                area_id=area_id,
+                area_name=area_name,
+                crop_id=crop_id,
+                crop_name=crop_name,
+                crop_specific=crop_specific,
+                daily_temp_max=daily_temp_max,
+                daily_temp_avg=daily_temp_avg,
+                daily_temp_min=daily_temp_min,
+                daily_rmh_avg=daily_rmh_avg,
+                daily_rmh_min=daily_rmh_min,
+                daily_precipitation=daily_precipitation,
+                daily_wind_speed_avg=daily_wind_speed_avg,
+                daily_accumulated_sunset=daily_accumulated_sunset
+            )
+            
+            CropEnvironment.objects.get_or_create(**db_prams)
